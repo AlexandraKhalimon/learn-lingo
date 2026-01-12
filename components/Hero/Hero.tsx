5f4c2f281dc68a2468ddba10cdc5b3dc3a54faa5
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-01-12 22:34:07 +0200

    style: add hero styles

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -5,9 +5,9 @@ export default function Hero() {
   return (
     <section className={css.section}>
       <div className={css.hero}>
-        <div>
+        <div className={css.infoContainer}>
           <h1 className={css.title}>
-            Unlock your potential with the best <span>language</span> tutors
+            Unlock your potential with the best <span>language </span> tutors
           </h1>
           <p className={css.info}>
             Embark on an Exciting Language Journey with Expert Language Tutors:
@@ -25,19 +25,19 @@ export default function Hero() {
       </div>
       <ul className={css.list}>
         <li>
-          <p className={css.number}>32,000 +</p>
+          <p className={css.number}>32,000+</p>
           <p className={css.text}>Experienced tutors</p>
         </li>
         <li>
-          <p className={css.number}>300,000 +</p>
+          <p className={css.number}>300,000+</p>
           <p className={css.text}>5-star tutor reviews</p>
         </li>
         <li>
-          <p className={css.number}>120 +</p>
+          <p className={css.number}>120+</p>
           <p className={css.text}>Subjects taught</p>
         </li>
         <li>
-          <p className={css.number}>200 +</p>
+          <p className={css.number}>200+</p>
           <p className={css.text}>Tutor nationalities</p>
         </li>
       </ul>
