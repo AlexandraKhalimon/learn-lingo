c0b7b042d541d417a161117fc5d66e69148df469
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-01-15 22:29:07 +0200

    chore: add teacher type

---
+++ types/teacher.ts
@@ -0,0 +1,32 @@
+export type Teacher = {
+  name: string;
+  surname: string;
+  languages: Language[];
+  levels: Level[];
+  rating: number;
+  reviews: Review[];
+  price_per_hour: number;
+  lessons_done: number;
+  avatar_url: string;
+  lesson_info: string;
+  conditions: Condition[];
+  experience: string;
+};
+
+export type Language = string;
+
+export type Level =
+  | 'A1 Beginner'
+  | 'A2 Elementary'
+  | 'B1 Intermediate'
+  | 'B2 Upper-Intermediate'
+  | 'C1 Advanced'
+  | 'C2 Proficient';
+
+export type Review = {
+  reviewer_name: string;
+  reviewer_rating: number;
+  comment: string;
+};
+
+export type Condition = string;
