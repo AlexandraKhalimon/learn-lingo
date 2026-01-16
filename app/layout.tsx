9ad2c2078c6e6a1ffdfe000153ecd5d15d4f5918
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-01-16 23:33:02 +0200

    chore: add and setup react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header/Header';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider.jsx';
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -46,8 +47,10 @@ export default function RootLayout({
       </head>
 
       <body className={`${roboto.variable}`}>
-        <Header />
-        {children}
+        <TanStackProvider>
+          <Header />
+          {children}
+        </TanStackProvider>
       </body>
     </html>
   );
