da97cd9808eea47fb4bb38ac9a54f84f84c708fe
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-01-20 22:18:35 +0200

    feat: implement registration and login with firebase api

--- lib/api.ts
+++ lib/api.ts
@@ -25,3 +25,74 @@ export const fetchTeachers = async ({
     id: key,
   }));
 };
+
+const API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+const registerEndpoint = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+const updateProfileEndpoint = `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`;
+
+interface RegisterProps {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface RegisterResponse {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  displayName?: string;
+}
+
+export async function registerUser({
+  name,
+  email,
+  password,
+}: RegisterProps): Promise<RegisterResponse> {
+  const { data } = await axios.post(registerEndpoint, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+
+  if (name) {
+    await axios.post(updateProfileEndpoint, {
+      idToken: data.idToken,
+      displayName: name,
+      returnSecureToken: true,
+    });
+  }
+
+  return data;
+}
+
+const loginEndpoint = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+
+interface LoginProps {
+  email: string;
+  password: string;
+}
+
+interface LoginResponse {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered: boolean;
+  displayName?: string;
+}
+
+export async function loginUser({
+  email,
+  password,
+}: LoginProps): Promise<LoginResponse> {
+  const { data } = await axios.post(loginEndpoint, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+
+  return data;
+}
